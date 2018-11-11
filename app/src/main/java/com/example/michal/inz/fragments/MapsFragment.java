210c9ae7ef79b5d13f3ae288b7e95090fcfcc4c0
Bebido <mariusz.pucilowski@gmail.com>
2018-11-11 13:35:37 +0100

    Pierwsza integracja z mapami

--- app/src/main/java/com/example/michal/inz/fragments/MapsFragment.java
+++ app/src/main/java/com/example/michal/inz/fragments/MapsFragment.java
@@ -2,18 +2,38 @@ package com.example.michal.inz.fragments;
 
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.michal.inz.R;
 
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.ExternalRenderTheme;
+
+import java.io.File;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapsFragment extends Fragment implements FragmentName {
+
+    private static final String MAP_FILE ="poland.map";
+    private MapView mapView;
+    public FragmentActivity activity;
+    public View view;
+
 
 
     public MapsFragment() {
@@ -24,9 +44,45 @@ public class MapsFragment extends Fragment implements FragmentName {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+
+
+        AndroidGraphicFactory.createInstance(getActivity().getApplication());
+        activity = getActivity();
+        view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        mapView = view.findViewById(R.id.openmapview);
+        mapView.setClickable(true);
+        mapView.getMapScaleBar().setVisible(true);
+        mapView.setBuiltInZoomControls(true);
+        try {
+
+            File mapFile = new File(Environment.getExternalStorageDirectory(), MAP_FILE);
+
+
+
+            MapDataStore mapDataStore = new MapFile(mapFile);
+            TileCache tileCache = AndroidUtil.createTileCache(this.getActivity(), "fragments",
+                    this.mapView.getModel().displayModel.getTileSize(), 1.0f, 1.5);
+
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+                    mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+            tileRendererLayer.setXmlRenderTheme(new ExternalRenderTheme(new File(Environment.getExternalStorageDirectory(), "theme.xml")));
+
+
+            this.mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+            mapView.setCenter(new LatLong(52.517037, 18.38886));
+            mapView.setZoomLevel((byte) 12);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return view;
     }
+
+
 
     @Override
     public String getName() {
