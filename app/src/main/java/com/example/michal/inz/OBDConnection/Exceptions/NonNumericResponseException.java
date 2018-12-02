ce9ac9f8cc7f0df641f388e4ab165a2dcebb2e61
Adam Jereczek <adam.jereczek@intel.com>
2018-12-02 13:42:06 +0100

    Zmiana w Exceptions

--- app/src/main/java/com/example/michal/inz/OBDConnection/Exceptions/NonNumericResponseException.java
+++ app/src/main/java/com/example/michal/inz/OBDConnection/Exceptions/NonNumericResponseException.java
@@ -1,6 +1,6 @@
 package com.example.michal.inz.OBDConnection.Exceptions;
 
-public class NonNumericResponseException extends ResponseException {
+public class NonNumericResponseException extends RuntimeException {
     public NonNumericResponseException(String message) {
         super("Error reading response: " + message);
     }
