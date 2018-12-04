aa2761176a2e58183c235d927fdeed6f104d76cc
Adam Jereczek <adam.jereczek@intel.com>
2018-12-04 22:57:41 +0100

    Uproszczenie klas

--- app/src/main/java/com/example/michal/inz/OBDConnection/Exceptions/UnsupportedCommandException.java
+++
@@ -1,7 +0,0 @@
-package com.example.michal.inz.OBDConnection.Exceptions;
-
-public class UnsupportedCommandException extends ResponseException {
-    public UnsupportedCommandException() {
-        super("7F 0[0-A] 1[1-2]", true);
-    }
-}
